2610f046af973d3db05fc86fd7afe3e141b8da23
Mario Oliver <mar.mariooliver93@gmail.com>
2023-08-22 13:07:16 -0400

    Added a category search functionality that pulls all categories from our prisma and gives access to teh server component. The server component passes the data to a clienjt component category that then utlizes searchParams and querystring to add the cateogry to our url. the cateogry component also populates all of our cateogrie sinto buttons that can be clicked to add the query parameters to the url connecting the frontend and backend

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,12 +1,17 @@
 import SearchInput from '@/components/SearchInput';
 import React from 'react';
+import prismadb from '@/lib/prismadb';
+import Categories from '@/components/Categories';
 
-const page = () => {
+//This is a server component so it has access to the database via utils
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany();
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 };
 
-export default page;
+export default RootPage;
