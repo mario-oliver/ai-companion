6f9aec46dc2099def0e63f6c61fb9ea54a9c6a62
Mario Oliver <mar.mariooliver93@gmail.com>
2023-08-24 11:23:46 -0400

    fixed security issue around the front end and backend allowing the updating of companion's that were not created by the user

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,24 +1,29 @@
 import CompanionForm from '@/components/CompanionForm';
 import prismadb from '@/lib/prismadb';
+import { auth, redirectToSignIn } from '@clerk/nextjs';
 import React from 'react';
 
 interface CompanionIdPageProps {
-  params: {
-    companionId: string;
-  };
+    params: {
+        companionId: string;
+    };
 }
 
 const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
-  //TODO: Check subscription status
+    //TODO: Check subscription status
+    const { userId } = auth();
 
-  const companion = await prismadb.companion.findUnique({
-    where: {
-      id: params.companionId,
-    },
-  });
+    if (!userId) return redirectToSignIn();
 
-  const categories = await prismadb.category.findMany();
-  return <CompanionForm initialData={companion} categories={categories} />;
+    const companion = await prismadb.companion.findUnique({
+        where: {
+            userId,
+            id: params.companionId,
+        },
+    });
+
+    const categories = await prismadb.category.findMany();
+    return <CompanionForm initialData={companion} categories={categories} />;
 };
 
 export default CompanionIdPage;
