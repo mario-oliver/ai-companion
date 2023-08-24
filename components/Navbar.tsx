91c69e1363554d17ffebe8e57a8fd3da9a6e9712
Mario Oliver <mar.mariooliver93@gmail.com>
2023-08-24 11:18:41 -0400

    fixed an error with signout taking us away from our webpage rather than to dashboard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,35 +13,36 @@ import { ModeToggle } from './theme-toggle';
 import MobileSidebar from './MobileSidebar';
 
 const fontMine = Poppins({
-  weight: '600',
-  subsets: ['latin'],
+    weight: '600',
+    subsets: ['latin'],
 });
 
 const Navbar = () => {
-  return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
-      <div className="flex items-center">
-        <MobileSidebar />
-        <Link href="/">
-          <h1
-            className={cn(
-              'hidden md:block text-xl md:text-3xl font-bold primary',
-              fontMine.className
-            )}
-          >
-            companion.ai
-          </h1>
-        </Link>
-      </div>
-      <div className="flex items-center gap-x-3">
-        <Button size="sm" variant="premium">
-          <Sparkles className="h-4 w-4 fill-white text-white" /> Upgrade
-        </Button>
-        <ModeToggle />
-        <UserButton></UserButton>
-      </div>
-    </div>
-  );
+    return (
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
+            <div className="flex items-center">
+                <MobileSidebar />
+                <Link href="/">
+                    <h1
+                        className={cn(
+                            'hidden md:block text-xl md:text-3xl font-bold primary',
+                            fontMine.className
+                        )}
+                    >
+                        companion.ai
+                    </h1>
+                </Link>
+            </div>
+            <div className="flex items-center gap-x-3">
+                <Button size="sm" variant="premium">
+                    <Sparkles className="h-4 w-4 fill-white text-white" />{' '}
+                    Upgrade
+                </Button>
+                <ModeToggle />
+                <UserButton afterSignOutUrl="/" />
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
