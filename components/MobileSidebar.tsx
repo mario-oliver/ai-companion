1c9d98a0ddc771f219fdaa3427156321b27fc3eb
Mario Oliver <mar.mariooliver93@gmail.com>
2023-08-28 19:23:21 -0400

    added ispro to mobilesidebar - bug

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -3,18 +3,20 @@ import { Menu } from 'lucide-react';
 
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import Sidebar from './Sidebar';
+import { checkSubscription } from '@/lib/subscription';
 
-const MobileSidebar = () => {
-  return (
-    <Sheet>
-      <SheetTrigger className="md:hidden pr-4">
-        <Menu />
-      </SheetTrigger>
-      <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
-      </SheetContent>
-    </Sheet>
-  );
+const MobileSidebar = async () => {
+    const isPro = await checkSubscription();
+    return (
+        <Sheet>
+            <SheetTrigger className="md:hidden pr-4">
+                <Menu />
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+                <Sidebar isPro={isPro} />
+            </SheetContent>
+        </Sheet>
+    );
 };
 
 export default MobileSidebar;
